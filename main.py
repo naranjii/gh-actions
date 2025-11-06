cc53e6b02707b3ffd0dbaf36a49c290b1c5e2334
Laranjeira <163648999+naranjii@users.noreply.github.com>
2025-11-06 13:54:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-import os 
+import os
+import requests
 
-segredo = os.environ.get("SEGREDO")
-print(segredo)
+# Acessando o segredo, que pode ser a URL do webhook
+webhook_url = os.getenv("SEGREDO")
+message = "Aqui está a mensagem que eu quero enviar para o Webhook!"
+
+# Enviando a mensagem para o webhook
+response = requests.post(webhook_url, json={"content": message})
+
+# Verificando a resposta
+if response.status_code == 200:
+    print("Mensagem enviada com sucesso!")
+else:
+    print(f"Erro ao enviar mensagem. Status code: {response.status_code}")
