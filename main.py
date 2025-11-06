09e282546745cf736530523dad6abf4ae26df644
Laranjeira <163648999+naranjii@users.noreply.github.com>
2025-11-06 13:58:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import requests
 
 # Acessando o segredo, que pode ser a URL do webhook
 webhook_url = "https://webhook.site/9ea504d7-0312-4901-ba0e-39904f70d515"
-message = os.envinron.get("SEGREDO")
-# Enviando a mensagem para o webhook
-response = requests.post(webhook_url, json={"content": message})
+message = os.getenv("SEGREDO")  # Acessando o segredo a partir da variável de ambiente
 
-# Verificando a resposta
-if response.status_code == 200:
-    print("Mensagem enviada com sucesso!")
+# Verificando se a variável 'message' foi obtida corretamente
+if message is None:
+    print("Erro: SEGREDO não encontrado nas variáveis de ambiente.")
 else:
-    print(f"Erro ao enviar mensagem. Status code: {response.status_code}")
+    # Enviando a mensagem para o webhook
+    response = requests.post(webhook_url, json={"content": message})
+
+    # Verificando a resposta
+    if response.status_code == 200:
+        print("Mensagem enviada com sucesso!")
+    else:
+        print(f"Erro ao enviar mensagem. Status code: {response.status_code}")
